bc7bc5fe13ba13607cd2f6f9f57877d63fd7126e
Miguel Dos Santos Carregal <migueldsc86@gmail.com>
2018-09-25 20:52:23 +0200

    Added CharacterDetail

---
+++ src/components/sections/characterDetail/view.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text, Image, TouchableOpacity} from 'react-native'
+import styles from './styles'
+
+export default class extends React.Component{
+    render(){
+        return (
+            <View style={styles.container}>
+            </View>
+        )
+    }
+}
